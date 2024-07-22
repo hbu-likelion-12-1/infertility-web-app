fb84f54ef2650a31ae36031a0e71453a7a2d8d9a
pingmong <kny7024@naver.com>
2024-07-23 01:47:21 +0900

    feat: Onboarding CSS 및 라우터 완료

--- src/pages/Onborading/Onboarding1.tsx
+++ src/pages/Onborading/Onboarding1.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '../../assets/styles/Onboarding.module.css';
+import onboardImage1 from '../../assets/images/Onboard1.svg';
 
 const Onboarding1: React.FC = () => {
     const navigate = useNavigate();
     return (
         <div className={styles.onboardingContainer}>
-            <div className={`${styles.backgroundImage}`} style={{ backgroundImage: "url('../../assets/images/logo.svg')" }}></div>
+            <div className={styles.backgroundImage1} style={{backgroundImage: `url(${onboardImage1})`}}></div>
             <div className={styles.textContainer}>
-                <h1 className={styles.title}>안녕하세요!</h1>
-                <p className={styles.subtitle}>AI 기반의 나의 맞춤형 마음 공유 서비스</p>
-                <p className={styles.description}>WEBLOOM에 오신 것을 환영합니다</p>
+                <p className={styles.intro1}>안녕하세요!</p>
+                <p className={styles.intro2}>AI기반<br/> 나의 맞춤형 마음 공유 서비스<br/>WEBLOOM에 오신 것을 환영합니다.</p>
+                <div className={styles.pagination}>
+                    <div className={`${styles.paginationDot} ${styles.active}`}></div>
+                    <div className={styles.paginationDot}></div>
+                    <div className={styles.paginationDot}></div>
+                    <div className={styles.paginationDot}></div>
+                </div>
+                <button className={styles.button} onClick={() => navigate('/onboarding2')}>다음</button>
             </div>
-            <div className={styles.pagination}>
-                <div className={`${styles.paginationDot} ${styles.active}`}></div>
-                <div className={styles.paginationDot}></div>
-                <div className={styles.paginationDot}></div>
-                <div className={styles.paginationDot}></div>
-            </div>
-            <button className={styles.button} onClick={() => navigate('/onboarding2')}>다음</button>
         </div>
     );
 };
--- src/pages/Onborading/Onboarding4.tsx
+++ src/pages/Onborading/Onboarding4.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '../../assets/styles/Onboarding.module.css';
+import onboardImage4 from '../../assets/images/Onboard4.svg';
 
 const Onboarding4: React.FC = () => {
     const navigate = useNavigate();
     return (
         <div className={styles.onboardingContainer}>
-            <div className={`${styles.image} ${styles.onboardImage4}`}></div>
+            <div className={styles.backgroundImage4} style={{backgroundImage: `url(${onboardImage4})`}}></div>
             <div className={styles.textContainer}>
-                <h1 className={styles.title}>지금 바로 가입하고,</h1>
-                <p className={styles.subtitle}>AI Bloom과 함께</p>
-                <p className={styles.description}>난임 극복을 위한 여정에 행복의 꽃을 피워요!</p>
+                <p className={styles.intro1}>지금 바로 가입하고,</p>
+                <p className={styles.intro2}>AI Bloom과 함께<br/>난임 극복을 위한 여정에<br/>행복의 꽃을 피워요!</p>
+                <div className={styles.pagination}>
+                    <div className={styles.paginationDot}></div>
+                    <div className={styles.paginationDot}></div>
+                    <div className={styles.paginationDot}></div>
+                    <div className={`${styles.paginationDot} ${styles.active}`}></div>
+                </div>
+                <button className={styles.button} onClick={() => navigate('/login')}>난임 극복 여정 시작하기</button>
             </div>
-            <div className={styles.pagination}>
-                <div className={styles.paginationDot}></div>
-                <div className={styles.paginationDot}></div>
-                <div className={styles.paginationDot}></div>
-                <div className={`${styles.paginationDot} ${styles.active}`}></div>
-            </div>
-            <button className={styles.button} onClick={() => navigate('/home')}>난임 극복 여정 시작하기</button>
         </div>
     );
 };
