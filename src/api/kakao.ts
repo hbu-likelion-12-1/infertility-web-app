30bb9152923f0fd78fb455cb310cdb3d737db304
galaxy4276 <deveungi@gmail.com>
2024-07-23 02:45:31 +0900

    refactor: url commoncase

--- src/api/kakao.ts
+++ src/api/kakao.ts
@@ -12,12 +12,14 @@ interface KakaoApi {
   login: (authCode: string) => Promise<LoginResponse>;
 }
 
+const ENDPOINT = '/user/auth';
+
 export const kakaoApi: KakaoApi = {
   // 카카오 서비스에서 로그인을 수행할 수 있는 (인증 코드를 발급받을 수 있는) URL 이 반환됩니다
   getKakaoPublishAuthCodeUrl() {
-    return apiClient.post('/api/user/auth/kakao/url');
+    return apiClient.post(`${ENDPOINT}/kakao/url`);
   },
   login(authCode) {
-    return apiClient.post(`/api/user/auth/login?code=${authCode}`);
+    return apiClient.post(`${ENDPOINT}/login?code=${authCode}`);
   }
 };
