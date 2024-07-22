5c805400f143bcc8c65996fc0c0c5523a6283fcb
galaxy4276 <deveungi@gmail.com>
2024-07-23 02:42:51 +0900

    feat: enum 작성

--- src/api/kakao.ts
+++ src/api/kakao.ts
@@ -2,17 +2,15 @@ import { apiClient } from './client';
 import { User } from '@/types/object';
 
 interface LoginResponse {
-  user: User
+  user: User;
   accessToken: string;
   kakaoId: string;
 }
-
 
 interface KakaoApi {
   getKakaoPublishAuthCodeUrl: () => Promise<string>;
   login: (authCode: string) => Promise<LoginResponse>;
 }
-
 
 export const kakaoApi: KakaoApi = {
   // 카카오 서비스에서 로그인을 수행할 수 있는 (인증 코드를 발급받을 수 있는) URL 이 반환됩니다
