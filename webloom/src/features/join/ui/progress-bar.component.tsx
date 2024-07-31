2ae8bd14b1ff23f13ebfd5001c84a625af14ddcd
galaxy4276 <deveungi@gmail.com>
2024-07-31 10:35:19 +0900

    feat: 진행 바 컴포넌트 작성

---
+++ webloom/src/features/join/ui/progress-bar.component.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { default as Bar } from "@ramonak/react-progress-bar";
+
+interface Props {
+  completed: number;
+}
+
+const ProgressBar: React.FC<Props> = ({ completed }) => {
+  return (
+    <Bar
+      completed={completed}
+      labelColor="#F3AF81"
+      bgColor="#FDEFE6"
+    />);
+};
+
+export default ProgressBar;
