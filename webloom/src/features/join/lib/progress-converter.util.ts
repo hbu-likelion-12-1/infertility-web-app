11e0a4f46fc15d4278f8fd1fc9bdc2630c893e70
galaxy4276 <deveungi@gmail.com>
2024-08-02 02:16:50 +0900

    fix: progress 상수 값 오류 수정

--- webloom/src/features/join/lib/progress-converter.util.ts
+++ webloom/src/features/join/lib/progress-converter.util.ts
@@ -1,6 +1,6 @@
 import { JoinPhase } from "@/features/join/constants/enum";
 
-const BASE = 7.6923076923;
+const BASE = 8.33333333;
 
 export default function convertProgress(phase: JoinPhase) {
   if (phase in JoinPhase) {
