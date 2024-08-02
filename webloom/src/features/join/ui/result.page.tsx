5bd058826175af9c4317146b37cc454fe0366f25
galaxy4276 <deveungi@gmail.com>
2024-08-02 21:48:51 +0900

    style: 우울증 테스트 화면 스타일 수정

--- webloom/src/features/join/ui/result.page.tsx
+++ webloom/src/features/join/ui/result.page.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+"use client";
+
+import React from "react";
 import useJoinStore from "@/features/join/lib/use-join.store";
 import clsx from "clsx";
 import Button from "@/shared/ui/button";
 import getDepressionResult from "@/shared/lib/depression";
+import { useRouter } from "next/navigation";
 
 const JoinResult = () => {
   const { depressionScore } = useJoinStore();
   const { title, content } = getDepressionResult(depressionScore);
+  const { push } = useRouter();
+
+  const onClickNext = () => push("/router");
 
   return (
     <article className={clsx([
@@ -19,19 +25,24 @@ const JoinResult = () => {
         <h3 className="text-primary-0 text-[36px] font-bold">{depressionScore}점</h3>
       </section>
 
-      <section className={clsx([
-        "flex flex-col items-center text-center",
-        "shadow-card w-full py-[28px]",
-        "rounded-[14px] border border-gray-100",
-      ])}>
-        <h4 className="text-[18px] font-bold text-primary-0 pb-[12px]">{title}</h4>
-        <span className="text-[16px]">{content}</span>
-      </section>
+      <nav className="w-full flex justify-center px-[32px] pt-[60px]">
+        <section className={clsx([
+          "flex flex-col items-center text-center",
+          "shadow-card w-full py-[28px] px-[32px]",
+          "rounded-[14px] border border-gray-100",
+        ])}>
+          <h4 className="text-[18px] font-bold text-primary-0 pb-[12px]">{title}</h4>
+          <span className="text-[16px] w-3/4">{content}</span>
+        </section>
+      </nav>
 
-      <section>
+      <dd className="flex-1"/>
+
+      <section className="mb-[60px] w-full">
         <Button
           theme="primary"
           className="w-full"
+          onClick={onClickNext}
         >
           고생하셨어요! 이제 마지막 단계만 남았어요.
         </Button>
