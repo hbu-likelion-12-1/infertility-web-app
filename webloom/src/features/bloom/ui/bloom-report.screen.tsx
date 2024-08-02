519032ebe02f482c5eb627479582b9eeb8bc1662
galaxy4276 <deveungi@gmail.com>
2024-08-03 08:20:36 +0900

    wip: bloom 탭에서 AI 피드백을 확인할 수 있다

---
+++ webloom/src/features/bloom/ui/bloom-report.screen.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import clsx from "clsx";
+
+import useFeedbackQuery from "@/features/bloom/query/get-feedback.query";
+import AppLoading from "@/shared/ui/loading/loading.component";
+
+
+const BloomReport = () => {
+  const { data: bloom, isLoading } = useFeedbackQuery();
+
+  const paragraphs = (() => {
+    if (!bloom?.feedback.content) return ["현재 피드백이 존재하지 않습니다."];
+    const feedback: string = bloom.feedback.content;
+    const feeds: string[] = feedback.split("\n");
+
+    return feeds;
+  })();
+
+  if (!bloom || isLoading) return <AppLoading/>;
+
+  return (
+    <>
+      <header className="w-full h-[42px] flex py-[16px] mb-[21px] my-[16px]">
+        <h1 className="text-[24px] font-bold">
+          Bloom의 응원과 조언
+        </h1>
+      </header>
+
+      <section
+        className={clsx([
+          "w-full py-[24px] px-[20px]",
+          "bg-primary-2 rounded-[24px]",
+          "flex flex-col"
+        ])}
+      >
+        {paragraphs.map((p, i) => (
+          <span className="text-[16px] leading-6" key={i}>
+            {p}
+          </span>
+        ))}
+
+      </section>
+    </>
+  );
+};
+
+export default BloomReport;
---
+++ webloom/src/features/bloom/ui/bloom.router.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { useState } from 'react';
+import { BloomTab } from "@/features/bloom/constant/enum";
+import BloomReport from "@/features/bloom/ui/bloom-report.screen";
+import clsx from "clsx";
+
+const BloomRouter = () => {
+  const [tab, setTab] = useState(BloomTab.REPORT);
+
+  return (
+    <article className="w-full h-full flex flex-col px-[20px]">
+      <section className="w-full h-[48px] flex">
+        <button
+          className={clsx([
+            "flex-1 text-center",
+            tab === BloomTab.REPORT && "text-primary-0 border-b-[3px] border-primary-0",
+            tab !== BloomTab.REPORT && "text-slate-300 border-b-[3px] border-slate-300"
+          ])}
+        >
+          Bloom 리포트
+        </button>
+
+        <button
+          className={clsx([
+            "flex-1 text-center",
+            tab === BloomTab.RECOMMENDATION && "text-primary-0 border-b-[3px] border-primary-0",
+            tab !== BloomTab.RECOMMENDATION && "text-slate-300 border-b-[3px] border-slate-300"
+          ])}
+        >
+          Bloom 추천
+        </button>
+      </section>
+
+      {tab === BloomTab.REPORT && <BloomReport/>}
+    </article>
+  );
+};
+
+export default BloomRouter;
