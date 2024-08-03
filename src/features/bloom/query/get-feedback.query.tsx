9855574a7d448ad58c83787b5423340e6840e549
galaxy4276 <deveungi@gmail.com>
2024-08-03 13:57:34 +0900

    refactor: 피드백 데이터는 주기적으로 새로 갱신한다

--- src/features/bloom/query/get-feedback.query.tsx
+++ src/features/bloom/query/get-feedback.query.tsx
@@ -5,7 +5,10 @@ import { Server } from "@/service/api";
 const useFeedbackQuery = () => {
   return useQuery({
     queryKey: ["bloom_feedback", "get"],
-    queryFn: Server.BloomAI.get
+    queryFn: Server.BloomAI.get,
+    refetchOnMount: true,
+    refetchOnWindowFocus: true,
+    refetchInterval: 30000,
   })
 };
 
