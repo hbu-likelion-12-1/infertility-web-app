042f21e68d4b9e810df4ffece0a67421385329e3
galaxy4276 <deveungi@gmail.com>
2024-08-04 16:36:58 +0900

    feat: 날짜 관련 유틸 작성

---
+++ src/shared/lib/day.ts
@@ -0,0 +1,19 @@
+import dayjs from "dayjs";
+
+const korDay: Record<number, string> = {
+  0: "월요일",
+  1: "화요일",
+  2: "수요일",
+  3: "목요일",
+  4: "금요일",
+  5: "토요일",
+  6: "일요일",
+};
+
+export const parseDayToKor = (day: number) => korDay[day];
+
+export const dayToDate = (d: string) => {
+  const o = dayjs(d);
+  const day = parseDayToKor(o.day());
+  return o.format("YY.MM.DD") + day;
+};
