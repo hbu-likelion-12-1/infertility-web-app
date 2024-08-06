1b9c61ad070e48ca00b3aee717955db31d513b1a
galaxy4276 <deveungi@gmail.com>
2024-08-06 23:22:14 +0900

    fix: suspense 제거

--- src/app/kakao/page.tsx
+++ src/app/kakao/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef, Suspense } from 'react';
+import React, { useEffect, useRef } from 'react';
 import useAuth from "@/shared/lib/use-auth.hook";
 import AppLoading from "@/shared/ui/loading/loading.component";
 import { useRouter, useSearchParams } from 'next/navigation';
@@ -39,9 +39,7 @@ const KakaoCallbackPage = () => {
   }, [login, router, searchParams]);
 
   return (
-    <Suspense>
-      <AppLoading/>
-    </Suspense>);
+    <AppLoading/>
 };
 
 export default KakaoCallbackPage;
